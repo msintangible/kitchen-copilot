ddf50c3dcabd3e6a1313bdd10892adb0b19dd724
Rehan Ananth <rehanananth@gmail.com>
2026-03-09 05:46:52 +0000

    Fixed indentation error in cloudbuild.yaml & firestore_client.py

--- backend/db/firestore_client.py
+++ backend/db/firestore_client.py
@@ -1,17 +1,17 @@
 from google.cloud import firestore
-from backend/config.py import PROJECT_ID, FIRESTORE_DATABASE
+from backend.config import PROJECT_ID, FIRESTORE_DATABASE
 
 db = firestore.AsyncClient(project=PROJECT_ID, database=FIRESTORE_DATABASE)
 
 async def get_recipe_by_id(recipe_id: str) -> dict:
 #TODO
-pass
+    pass
 
 async def search_recipes(query: str) -> list:
 #TODO
-pass
+    pass
 
 async def get_all_recipes() -> list:
 #TODO
-pass
+    pass
 
